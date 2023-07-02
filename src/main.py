b99313e38ff33a6e95f0b6f84b35ed304b3deee2
cctong-castiel <castieltong@gmail.com>
2023-07-02 10:40:11 +0800

    added performance comparison

--- src/main.py
+++ src/main.py
@@ -1,17 +1,36 @@
 import ctypes
+import time
+import re
 
 if __name__ == '__main__':
     
     print("opening liblibrary.so")
     my_so = ctypes.cdll.LoadLibrary('../lib/libregexc.so')
-    print("loaded my_so")
-        
+    
+    print("loaded my_so")    
     my_so.replaceAll.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
 
-    print(f".so object: {my_so}")
-    print("call print_value function")
+    print(f".so object: {my_so}\n")
     input = b"Hello, world! I am happy today. Hello, world! Hello, world! Save the World"
     pattern = b"world"
     replacement = b"there"
-    my_so.replaceAll(input, pattern, replacement)
+    
+    print("call replaceAll function for 10000 times")
+    start = time.time()
+    for i in range(10000):
+        input = b"Hello, world! I am happy today. Hello, world! Hello, world! Save the World"
+        my_so.replaceAll(input, pattern, replacement)
     print(input)
+    print(f"time used for c++ replaceAll function: {time.time() - start}\n")
+    
+    print("call python regex replace function for 10000 times")
+    input = "Hello, world! I am happy today. Hello, world! Hello, world! Save the World"
+    pattern = "world"
+    replacement = "there"
+    
+    start = time.time()
+    for i in range(10000):
+        input = "Hello, world! I am happy today. Hello, world! Hello, world! Save the World"
+        input = re.sub(pattern, replacement, input)
+    print(input)
+    print(f"time used for python replace function: {time.time() - start}\n")
